5c332216f8390d2e414f92f36a6ecbfa8ed8a3ed
devgenx <sebastian110420@gmail.com>
2022-12-15 15:16:24 -0500

    Update app.js

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,116 +1,79 @@
-// install -> import --> use 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import AppRouter from './routers/AppRouter';
-import { addExpense } from './actions/expenses'
-import configureStore from './store/configureStore';
-import { setTextFilter } from './actions/filters'; 
-import getVisibleExpenses from './selectors/expenses'
+// install -> import --> use
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import AppRouter from "./routers/AppRouter";
+import { addExpense } from "./actions/expenses";
+import configureStore from "./store/configureStore";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
 
-
-import 'normalize.css/normalize.css';
-import './styles/styles.scss' 
-import 'react-dates/lib/css/_datepicker.css'
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
 
-store.dispatch(addExpense( {description: 'Water bill', amount: 4500 }))
-store.dispatch(addExpense( {description: 'Gas bill', createdAt: 1000 }))
-store.dispatch(addExpense( {description: 'Rent bill', amount: 109500 }))
+store.dispatch(addExpense({ description: "Water bill", amount: 4500 }));
+store.dispatch(addExpense({ description: "Gas bill", createdAt: 1000 }));
+store.dispatch(addExpense({ description: "Rent bill", amount: 109500 }));
 
-
-
-const state = store.getState()
+const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
-
-
 const jsx = (
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>
-)
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
 
+ReactDOM.render(jsx, document.getElementById("app"));
 
-ReactDOM.render(jsx, document.getElementById('app'));
- 
+// class Header extends React.Component {
+//    render() {
+//       return (
+//          <div>
+//             <h1>{this.props.title}</h1>
+//             <h2>{this.props.subtitle}</h2>
+//          </div>
+//       )
+//    }
+// }
 
+// class Action extends React.Component {
+//    render() {
+//       return (
+//          <div>
+//             <button
+//              onClick={this.props.handlePick}
+//             disabled={!this.props.hasOptions}
+//             >
+//             What should I do?
+//             </button>
+//          </div>
+//       )
+//    }
+// }
 
+// class Options extends React.Component {
+//    render() {
+//       return (
+//          <div>
+//          <button onClick={this.props.handleDeleteOptions}>Remove All</button>
+//          {
+//             this.props.options.map((option) => <Option key={option} optionText={option}/>)
+//          }
+//          </div>
+//       )
+//    }
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- // class Header extends React.Component {
- //    render() {
- //       return (
- //          <div>
- //             <h1>{this.props.title}</h1>
- //             <h2>{this.props.subtitle}</h2>
- //          </div>
- //       )
- //    }
- // }
- 
- 
- // class Action extends React.Component {
- //    render() {
- //       return (
- //          <div>
- //             <button
- //              onClick={this.props.handlePick}
- //             disabled={!this.props.hasOptions}
- //             >
- //             What should I do?
- //             </button>
- //          </div>
- //       )
- //    }
- // }
- 
- 
- 
- 
- // class Options extends React.Component {
- //    render() {
- //       return (
- //          <div>
- //          <button onClick={this.props.handleDeleteOptions}>Remove All</button>
- //          {
- //             this.props.options.map((option) => <Option key={option} optionText={option}/>)
- //          }
- //          </div>
- //       )
- //    }
- // }
-
- // class Option extends React.Component { 
- //    render() {
- //       return (
- //       <div>
- //          {this.props.optionText}
- //       </div>
- //       )
- //    }
- // }
- 
- 
+// class Option extends React.Component {
+//    render() {
+//       return (
+//       <div>
+//          {this.props.optionText}
+//       </div>
+//       )
+//    }
+// }
